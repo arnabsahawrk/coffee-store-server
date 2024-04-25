23d58c927521cd64146c64959be35f89bd8be684
arnabw <arnabsahawrk@gmail.com>
2024-04-25 15:26:24 +0600

    POST method include

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 //config
 require("dotenv").config();
@@ -11,7 +12,31 @@ app.use(cors());
 app.use(express.json());
 
 //Database Authenticate
-const uri = "mongodb://localhost:27017";
+// const uri = "mongodb://localhost:27017";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2p5zaxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const coffeeCollection = client.db("usersDB").collection("coffees");
+
+    //POST
+    app.post("/coffees", async (req, res) => {
+      const result = await coffeeCollection.insertOne(req.body);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send(`Server is running on ${port}`);
